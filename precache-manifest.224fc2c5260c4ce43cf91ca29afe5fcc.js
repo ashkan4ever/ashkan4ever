4052df5d550e507a6f1e7b472a2d7b0b918d17f9
Mohamad Zohrabi <m.zohrabi85@gmail.com>
2020-10-14 16:05:36 +0330

    Updates

---
+++ precache-manifest.224fc2c5260c4ce43cf91ca29afe5fcc.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c1f4440e43a1ef58635beff53c23653",
+    "url": "/ashkan4ever/index.html"
+  },
+  {
+    "revision": "f621ae667cd9e78ac225",
+    "url": "/ashkan4ever/static/css/2.dc17f35a.chunk.css"
+  },
+  {
+    "revision": "feee17ef2c98c7019004",
+    "url": "/ashkan4ever/static/css/main.57b6c2a6.chunk.css"
+  },
+  {
+    "revision": "f621ae667cd9e78ac225",
+    "url": "/ashkan4ever/static/js/2.ebb46a66.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ashkan4ever/static/js/2.ebb46a66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feee17ef2c98c7019004",
+    "url": "/ashkan4ever/static/js/main.9e6ac3f9.chunk.js"
+  },
+  {
+    "revision": "69cdbba449d3d16f32df",
+    "url": "/ashkan4ever/static/js/runtime-main.561814d1.js"
+  },
+  {
+    "revision": "8a207e3e0b7765af8f37edc5130dd517",
+    "url": "/ashkan4ever/static/media/24metrics.8a207e3e.png"
+  },
+  {
+    "revision": "cd46d678c21b1a125661b44810acf6eb",
+    "url": "/ashkan4ever/static/media/ariana.cd46d678.png"
+  },
+  {
+    "revision": "443d513381210f35fdd20ee8d1fd01bd",
+    "url": "/ashkan4ever/static/media/ingrow.443d5133.png"
+  },
+  {
+    "revision": "6076da3c93d91bf226ed0b16347f2a1e",
+    "url": "/ashkan4ever/static/media/ingrowT.6076da3c.jpg"
+  },
+  {
+    "revision": "0cbaaf9e7c6d5bc44d2d23665b02f019",
+    "url": "/ashkan4ever/static/media/nader.0cbaaf9e.jpeg"
+  },
+  {
+    "revision": "fad20cd35cb403bb669effc0e0f831eb",
+    "url": "/ashkan4ever/static/media/profile.fad20cd3.jpeg"
+  },
+  {
+    "revision": "d156bd1a4c9477a320f06c42e159a5ea",
+    "url": "/ashkan4ever/static/media/renderprops.d156bd1a.webp"
+  },
+  {
+    "revision": "3917e303ae14552e70dea4bc8c64e093",
+    "url": "/ashkan4ever/static/media/skill.3917e303.jpg"
+  },
+  {
+    "revision": "da370a8b38e47c6eabfc7417d2a14e8d",
+    "url": "/ashkan4ever/static/media/talentBridge.da370a8b.png"
+  },
+  {
+    "revision": "0a820cc80f2babaa9a62a76b32644225",
+    "url": "/ashkan4ever/static/media/zIndex.0a820cc8.png"
+  }
+]);
